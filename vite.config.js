1e1175a3c21b41dfd794a0f3307dcda7a5755adb
MrsRafiaShahid <mrssrafiashahid@gmail.com>
2025-07-19 01:24:20 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base:process.env.VITE_BASE_PATH || "/Portfolio",
+  base:process.env.VITE_BASE_PATH || "/Portfolio",
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
