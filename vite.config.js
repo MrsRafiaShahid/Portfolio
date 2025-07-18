93fc28db7c80d4c6335d3df7a75a9e10c4413b12
MrsRafiaShahid <mrssrafiashahid@gmail.com>
2025-07-19 01:48:11 +0500

    update title

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
